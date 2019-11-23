4a2bab936c33ca8fbde78a51092c40fd7b0dedb4
Davor Penzar <davor.penzar@gmail.com>
2019-11-23 18:20:08 +0100

    Opis za prevodenje

--- code/polygon/compile.sh
+++ code/polygon/compile.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+##  Compile a C or a C++ code using GCC.
+##
+##  Possible usage:
+##     ./compile code.c -o program
+##  where "code.c" is the path to the source code of the program, and "program"
+##  is the path to the resulting runnable file (compiled program).
+##
+##  The code is compiled with flags:
+##      -std=...            --  standard of the language,
+##      -pedantic-errors    --  all deviations from the standard result in an
+##                              error,
+##      -Wall               --  print all warnings,
+##      -O                  --  optimisation level 1
+##      -lm                 --  link mathematical functions (math.h or cmath),
+##      -pthread            --  compile with pthread.h functions.
+##
+
 # Compile the C code.
 gcc -std=c89 -pedantic-errors -Wall -O $@ -lm -pthread
 #gcc -std=c90 -pedantic-errors -Wall -O $@ -lm -pthread
